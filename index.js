67ecd7733de69f8e12d5ef667a0e2f3f8f1e55a7
Alvinkimutai <alvinchepkiyeng@gmail.com>
2024-06-30 15:51:50 +0300

    practices arrow functions

---
+++ index.js
@@ -0,0 +1,13 @@
+//Function expression to divide 2000 by 100
+const divide = function(a, b){
+    return (a/b);
+}
+console.log(divide(2000, 100));
+
+//square arrow function
+const square = x => x**2;
+   console.log(square(2));
+
+//addition arrow function
+   const add =(a, b) => a+b;
+console.log(add(3, 4));    
